7ec6beace51813e4a64cb77de04d00777c51d5b5
sneha <snehakalal816@gmail.com>
2026-01-06 15:33:48 +0530

    some commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <>
+    <h1>About us</h1>
+    <p>Welcome to our eCommerce platform, your trusted destination for convenient, secure, and enjoyable online shopping. We are dedicated to bringing you a wide range of quality products at affordable prices, all in one place.
+Our website is designed with user-friendly navigation, secure payment options, and fast order processing to ensure a smooth shopping experience for every customer. Whether you are looking for daily essentials, fashion items, electronics, or special products, we aim to meet your needs with reliability and ease.</p>
+<h1>Our Vision</h1>
+<p>We aspire to grow as a trusted online marketplace by continuously improving our services, embracing new technologies, and building long-term relationships with our customers.
+Thank you for choosing our eCommerce website. We look forward to serving you and making your online shopping experience better every day. 🛒✨</p>
+    </>
   )
 }
 
